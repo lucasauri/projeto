a2692c1e28f9ef6890d6231c3de56ce6303c926d
lucasauri <lucasauri1@gmail.com>
2025-04-07 06:56:39 -0300

    Sistema piloto subindo

---
+++ sistemapiloto/sistema/src/main/java/sistech/example/sistema/service/HelloWorldService.java
@@ -0,0 +1,13 @@
+package sistech.example.sistema.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+
+ public String helloWorld(String name) {
+     return "Hello World" + name;
+ }
+
+}
